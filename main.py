ee1b6860db41d52329505d676f469b7911a96651
SalzDevs <a4000@alunos.isel.pt>
2026-06-15 23:20:23 +0100

    refactor: move add operation into Value class and make it mutate state

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ class Value:
         self.operations = []
 
 
-def add(val1:Value, val2:Value) -> Value:
-    val = Value(val1.data+val2.data)
-    val.operations.append(str(val1.data) + "+" + str(val2.data))
-    return val
+    def add(self,val1, val2):
+        self.operations.append(str(val1.data) + "+" + str(val2.data))
+        self.data = val1.data+val2.data
 
 x = Value(7)
 y = Value(3)
 
-z = add(x,y)
-z = add(z,Value(1))
+z = Value(0) 
+z.add(x,y)
+z.add(z,Value(1))
 
 print(z.data)
 print(z.operations)
