8276f8ed4e57192703037bff7d6a12c27af9e9a2
Ricardo <ricardoceia@MacBook-Air-de-Ricardo.local>
2026-06-15 23:12:52 +0100

    add operation when add oper happens

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ class Value:
 
 
 def add(val1:Value, val2:Value) -> Value:
-    return Value(val1.data+val2.data)
+    val = Value(val1.data+val2.data)
+    val.operations.append(str(val1.data) + "+" + str(val2.data))
+    return val
 
 x = Value(7)
 y = Value(3)
 
 z = add(x,y)
+z = add(z,Value(1))
 
 print(z.data)
+print(z.operations)
