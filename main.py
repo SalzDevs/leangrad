0b751e14775a316880e85cb3ff6e121cb5cd8e71
SalzDevs <a4900@alunos.isel.pt>
2026-06-15 23:25:04 +0100

    feat(core): add multiplication operation to Value

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ class Value:
         self.operations.append(str(val1.data) + "+" + str(val2.data))
         self.data = val1.data+val2.data
 
+    def mul(self,val1,val2):
+        self.operations.append(str(val1.data) + "*" + str(val2.data))
+        self.data = val1.data*val2.data
+
 x = Value(7)
 y = Value(3)
 
 z = Value(0) 
 z.add(x,y)
 z.add(z,Value(1))
+z.mul(z,Value(2))
 
 print(z.data)
 print(z.operations)
