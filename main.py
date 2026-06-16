df2f72cf6bacb3667e6f266db172d296fdf01d72
SalzDevs <a49000@alunos.isel.pt>
2026-06-16 12:26:31 +0100

    refactor(core): introduce Operation class and start structuring computation graph

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
+class Operation:
+    def __init__(self,var1,var2,oper):
+        self.var1 = var1
+        self.var2 = var2
+        self.oper = oper
+
+    def __str__(self):
+        return f"{self.var1} {self.oper} {self.var2}"
+
 class Value:
-    def __init__(self,data:int):
+    def __init__(self,data:int,varName:str):
         self.data = data
         self.operations = []
+        self.varName = varName
 
 
     def add(self,val1, val2):
-        self.operations.append(str(val1.data) + "+" + str(val2.data))
+        operation = Operation(val1.varName,val2.varName,"+")
+        self.operations.append(str(operation))
         self.data = val1.data+val2.data
 
     def mul(self,val1,val2):
-        self.operations.append(str(val1.data) + "*" + str(val2.data))
-        self.data = val1.data*val2.data
-    
+        #self.operations.append()str(val1.data) + "-" + str(val2.data))
+        #self.data = val1.data*val2.data
+        pass
     def sub(self,val1,val2):
-        self.operations.append(str(val1.data) + "-" + str(val2.data))
-        self.data = val1.data-val2.data 
+        #self.operations.append(str(val1.data) + "-" + str(val2.data))
+        #self.data = val1.data-val2.data
+        pass
 
-x = Value(7)
-y = Value(3)
+#init (x,y,z)
+x = Value(7,'x')
+y = Value(3,'y')
+z = Value(0,'z') 
 
-z = Value(0) 
+#operations on z
 z.add(x,y)
-z.add(z,Value(1))
-z.mul(z,Value(2))
-z.sub(z,Value(1))
 
 print(z.data)
 print(z.operations)
